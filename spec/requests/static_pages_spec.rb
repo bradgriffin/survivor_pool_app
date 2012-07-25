ece1ac61e33590d9b70ba96f8959509566f083db
Brad Griffin <bgriffin20@gmail.com>
2012-07-24 20:50:06 -0500

    Refactor static pages tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,40 +4,55 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Survivor Pool') }
-    it { should have_selector('title', text: full_title('')) }
+    let(:heading)    { 'Survivor Pool' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    let(:heading)    { 'Contact Us' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Terms page" do
     before { visit terms_path }
 
-    it { should have_selector('h1',    text: 'Terms') }
-    it { should have_selector('title', text: full_title('Terms')) }
+    let(:heading)    { 'Terms of Use' }
+    let(:page_title) { 'Terms' }
+
+    it_should_behave_like "all static pages"
   end
 
   it "should have the right links on the layout" do
