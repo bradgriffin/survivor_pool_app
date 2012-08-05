d6e3b329068794bb82fd7b89d1c1c4de8da623be
Brad Griffin <bgriffin20@gmail.com>
2012-08-05 16:42:52 -0500

    Refactor Team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -18,17 +18,11 @@ class Team < ActiveRecord::Base
 	include ActiveModel::Validations
 	attr_accessible :location, :nickname, :division, :coach, :wins_2011, :losses_2011, :wins_2012, :losses_2012
 
-	validates :location, presence: true
-	validates :nickname, presence: true, uniqueness: { case_sensitive: false }
-	validates :division, presence: true
-	validates :coach, presence: true, uniqueness: { case_sensitive: false }
-	validates :wins_2011, presence: true, :numericality => { :less_than_or_equal_to => 16 }
-	validates :losses_2011, presence: true, :numericality => { :less_than_or_equal_to => 16 }
-	validates :wins_2012, presence: true, :numericality => { :less_than_or_equal_to => 16 }
-	validates :losses_2012, presence: true, :numericality => { :less_than_or_equal_to => 16 }
-	
+	validates :location, :nickname, :division, :coach, :wins_2011, :losses_2011, 
+				:wins_2012, :losses_2012, presence: true
+	validates :nickname, :coach, uniqueness: { case_sensitive: false }
+	validates :wins_2011, :losses_2011, :wins_2012, :losses_2012, :numericality => { :less_than_or_equal_to => 16 }
 	validates_with TotalGamesValidator, :fields => [:wins_2011, :losses_2011]
-
 end
 
 
