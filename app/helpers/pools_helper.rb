c22bf2153656f663b905db8cbac6340474dbc2c6
Brad Griffin <bgriffin20@gmail.com>
2012-08-08 22:46:28 -0500

    Add method to automatically assign the user who creates a group as a member of that group

--- app/helpers/pools_helper.rb
+++ app/helpers/pools_helper.rb
@@ -1,2 +1,8 @@
 module PoolsHelper
+	def auto_membership(pool)
+		@user = current_user
+    	pool = Pool.find_by_name(pool.name)
+      	@membership = @pool.memberships.build("pool_id" => pool.id, "user_id" => @user.id)
+      	@membership.save
+	end
 end
