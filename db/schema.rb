8553bb438cc07cee3097f5f1f889ea7fcde79cb2
Brad Griffin <bgriffin20@gmail.com>
2012-08-08 22:43:11 -0500

    Updated schema from entries model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120806021914) do
+ActiveRecord::Schema.define(:version => 20120809004041) do
+
+  create_table "entries", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "pool_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "memberships", :force => true do |t|
     t.integer  "pool_id"
